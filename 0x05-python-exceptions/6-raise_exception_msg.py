4c5432ed87da661133a1f0b42f27ab5faaebb6fe
yonasghiwot <yonihabeshaw@gmail.com>
2024-04-02 01:20:31 +0300

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,7 +2,9 @@
 
 def raise_exception_msg(message=""):
    """function that raises a name exception with a message.
+    
     You are not allowed to import any module
+    
     Args:
         message: string of message to be raised
     """
