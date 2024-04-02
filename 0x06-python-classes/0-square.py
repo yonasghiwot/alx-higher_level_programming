d0013656c4b5b9fb0f621ac9da27c8342354af6b
yonasghiwot <yonihabeshaw@gmail.com>
2024-04-03 02:00:56 +0300

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""an empty class Square that defines a square"""
+"""not allowed to import any module"""
+
+
+class Square:
+    """Class that defines properties of square.
+    """
+    pass
